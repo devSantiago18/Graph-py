0c480e6caa7fe860936b28c092a82db9eaa4e2bc
DevTony101 <tonycortesano@gmail.com>
2020-07-27 11:49:07 -0500

    Deleting create_graph function

--- graph.py
+++ graph.py
@@ -36,34 +36,3 @@ class Graph:
         # Display
         plt.show()
 
-def create_graph():
-    graph = nx.Graph()
-    # read file, create nodes 
-    nodes = []
-    with open('nodes.txt','r') as file:
-        for line in file.readlines():
-            if line != "": 
-                nodes.append(line.replace("\n",""))
-    graph.add_nodes_from(nodes)
-
-    num_random = []
-    for i in range(len(nodes)):
-        """ 
-        if i < len(nodes)-1:
-            edge = (nodes[i],nodes[i+1])
-            graph.add_edge(*edge)
-        """     
-        ran = randrange(len(nodes))
-        if ran not in num_random:
-            num_random.append(ran)
-            edge = (nodes[i],nodes[ran])
-            graph.add_edge(*edge)
-        
-    print("Nodes of graph: ")
-    print(graph.nodes())
-    print("Edges of graph: ")
-    print(graph.edges())
-
-    nx.draw(graph)
-    #plt.savefig("simple_path.png") # save as png
-    plt.show() # display
