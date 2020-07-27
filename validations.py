7e08b50421dfe32fe928ccddd5c868198fa6f19c
DevTony101 <tonycortesano@gmail.com>
2020-07-27 12:37:04 -0500

    Reformating

--- validations.py
+++ validations.py
@@ -8,12 +8,13 @@ CACHE = list()
 PATTERN_USER = re.compile('^[a-zA-Z\d]{5,}$')
 PATTERN_PASSWORD = re.compile('^([A-Z]+)([a-zA-Z\d]+)$')
 
+
 def validation_data(user_response, password_response):
     if PATTERN_USER.search(user_response) == None or PATTERN_PASSWORD.search(password_response) == None:
-        messagebox.showinfo('user',"Credenciales no válidas")    
+        messagebox.showinfo('user', "Credenciales no válidas")
     else:
-        messagebox.showinfo('user',"Usuario registrado")    
-        with open("./docs/nodes.txt","a+") as file:
+        messagebox.showinfo('user', "Usuario registrado")
+        with open("./docs/nodes.txt", "a+") as file:
             user = User(user_response, password_response)
             num = randrange(1000)
             while num in CACHE:
@@ -23,4 +24,3 @@ def validation_data(user_response, password_response):
             print(user_response)
             print(user)
             file.write("{}\n".format(user.username))
-
--- view.py
+++ view.py
@@ -10,51 +10,50 @@ from graph import Graph
 HEIGHT_W = 30
 WIDTH_W = 200
 
+
 class Window(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.config(bd=15)
-        self.resizable(0,0)
+        self.resizable(0, 0)
         self.title('User validations')
 
-        frame = Frame(self , width=430 , height=280)
-        frame.pack(fill='both' , expand=1)
+        frame = Frame(self, width=430, height=280)
+        frame.pack(fill='both', expand=1)
         frame.config(bg='black')
         frame.config(bd=25)
         self.user_response = StringVar()
         self.password_response = StringVar()
 
-        userlabel = Label(frame,text='Username')
-        userlabel.place(x=15, y=10, width=WIDTH_W , height=HEIGHT_W)
+        userlabel = Label(frame, text='Username')
+        userlabel.place(x=15, y=10, width=WIDTH_W, height=HEIGHT_W)
         userlabel.config()
 
-        user_entry = Entry(frame,textvariable=self.user_response)
-        user_entry.place(x=15, y=50, width=WIDTH_W , height=HEIGHT_W)
+        user_entry = Entry(frame, textvariable=self.user_response)
+        user_entry.place(x=15, y=50, width=WIDTH_W, height=HEIGHT_W)
 
-        passwordlabel = Label(frame,text='Password',)
-        passwordlabel.place(x=15, y=100, width=WIDTH_W , height=HEIGHT_W)
+        passwordlabel = Label(frame, text='Password',)
+        passwordlabel.place(x=15, y=100, width=WIDTH_W, height=HEIGHT_W)
         passwordlabel.config()
 
-        password_entry = Entry(frame,textvariable=self.password_response)
-        password_entry.place(x=15, y=140, width=WIDTH_W , height=HEIGHT_W)
+        password_entry = Entry(frame, textvariable=self.password_response)
+        password_entry.place(x=15, y=140, width=WIDTH_W, height=HEIGHT_W)
         password_entry.config(show="*")
-        
-        btn_submit = Button(frame, text='Submit' , command=self.submit_data )
-        btn_submit.place(x=15, y=210,width=WIDTH_W , height=HEIGHT_W)
 
-        btn_show = Button(frame,text="Show graph",command=self.create_graph)
-        btn_show.place( x=225, y=210, width=170 , height=HEIGHT_W)
+        btn_submit = Button(frame, text='Submit', command=self.submit_data)
+        btn_submit.place(x=15, y=210, width=WIDTH_W, height=HEIGHT_W)
+
+        btn_show = Button(frame, text="Show graph", command=self.create_graph)
+        btn_show.place(x=225, y=210, width=170, height=HEIGHT_W)
 
     def submit_data(self):
-        validation_data(self.user_response.get(),self.password_response.get())
+        validation_data(self.user_response.get(), self.password_response.get())
         self.user_response.set("")
         self.password_response.set("")
-    
+
     def create_graph(self):
         graph = Graph()
         graph.create_nodes_from_file("./docs/nodes.txt")
         graph.generate_edges()
         print(graph)
         graph.draw()
-
-    
