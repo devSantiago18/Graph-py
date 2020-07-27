fd5e4bd96accd83e0b8f32632b7b883e89dba0dc
DevTony101 <tonycortesano@gmail.com>
2020-07-27 12:36:43 -0500

    Refactoting generate_edges function

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from random import randrange
 
+
 class Graph:
     def __init__(self):
         self.graph = nx.Graph()
@@ -12,27 +13,24 @@ class Graph:
         return f"Nodes: {self.graph.nodes()}. Edges: {self.graph.edges()}."
 
     def create_nodes_from_file(self, path):
-        with open(path,'r') as file:
+        with open(path, 'r') as file:
             for line in file.readlines():
-                if line != "": 
-                    self.nodes.append(line.replace("\n", ""))     
+                if line != "":
+                    self.nodes.append(line.replace("\n", ""))
         self.graph.add_nodes_from(self.nodes)
-    
+
     def generate_edges(self):
-        random_numbers = list()
-        i = 0
-        while i < len(self.nodes):
-            num = randrange(len(nodes))
-            if num not in random_numbers:
-                random_numbers.append(num)
-                edge = (self.nodes[i], self.nodes[num])
+        generated = []
+        for i in range(len(self.nodes)):
+            index = randrange(len(self.nodes))
+            if index not in generated:
+                generated.append(index)
+                edge = (self.nodes[i], self.nodes[index])
                 self.graph.add_edge(*edge)
-                i = i + 1
-    
-    def draw(self, path = None):
+
+    def draw(self, path=None):
         nx.draw(self.graph)
         # Save as a png file
-        #plt.savefig("simple_path.png")
+        # plt.savefig("simple_path.png")
         # Display
         plt.show()
-
