c7874e1c1721645084849e67051bf0cbbb24c178
DevTony101 <tonycortesano@gmail.com>
2020-07-29 12:52:39 -0500

    Refactor create_nodes_from_file function

--- graph.py
+++ graph.py
@@ -13,12 +13,13 @@ class Graph:
         return f"Nodes: {self.graph.nodes()}. Edges: {self.graph.edges()}."
 
     def create_nodes_from_file(self, path):
-        with open(path, newline='') as file:
-            reader = csv.reader(file, delimiter=' ')
+        with open(path, mode="r") as file:
+            reader = csv.reader(file, delimiter=',')
             for row in reader:
-                user, passw, _id, _next = row
-                self.graph.add_edge(_id,_next)
+                _id, user, pw, *adjacents = row
                 self.graph.add_node(_id)
+                for adjacent in adjacents:
+                    self.graph.add_edge(_id, adjacent)
 
     def draw(self, path=None):
         print(self.graph.nodes)
