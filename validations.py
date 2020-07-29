4308494a93fc92cc2e4fd5f04e04586711c70896
DevTony101 <tonycortesano@gmail.com>
2020-07-29 12:17:06 -0500

    Refactoring init_cache, validation_data functions.

--- validations.py
+++ validations.py
@@ -5,23 +5,18 @@ from modelUser import User
 from tkinter import messagebox
 
 # Constants
-#CACHE = list()
-PATH_CSV = "./docs/nodes.csv" 
+PATH_CSV = "./docs/nodes.csv"
 PATTERN_USER = re.compile('^[a-zA-Z\d]{5,}$')
 PATTERN_PASSWORD = re.compile('^([A-Z]+)([a-zA-Z\d]+)$')
 
-def _init_cache() -> tuple:
-    with open(PATH_CSV, newline='') as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        user = User()
-        for row in reader:
-            print(row)
-            if row == None or row == []:
-                return ([],[])
-            else:
-               *_, user.id , _ = row
-        return ([user],[user.id])
 
+def init_cache() -> list:
+    # Returns a list of saved ids
+    cache = []
+    with open(PATH_CSV, mode="r") as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        cache = [row[0] for row in reader]
+    return cache
 
 
 def validation_data(user_response, password_response):
@@ -29,16 +24,13 @@ def validation_data(user_response, password_response):
         messagebox.showinfo('user', "Credenciales no válidas")
     else:
         messagebox.showinfo('user', "Usuario registrado")
-        # CACHE,CACHE_ID = _init_cache()
-        # print(CACHE,CACHE_ID)
-        with open(PATH_CSV, mode="a", newline='') as csvfile:
+        cache = init_cache()
+        print(cache)
+        with open(PATH_CSV, mode="a+", newline='') as csvfile:
             file_writer = csv.writer(csvfile, delimiter=',')
-            user = User(1, user_response, password_response)
-            num = randrange(1000)
-            # while num in CACHE_ID:
-            #     num = randrange(1000)
-            # user.id = num
-            # user.next_node = num if not CACHE else CACHE[len(CACHE)-1].id 
-            # CACHE.append(user)
+            num_id = randrange(1000)
+            while num_id in cache:
+                num_id = randrange(1000)
+            user = User(num_id, user_response, password_response)
             print(user)
             file_writer.writerow(user.to_list())
