c1d764a7eb9b241be3e4a8db4d922f9996a28e11
DevTony101 <tonycortesano@gmail.com>
2020-07-29 11:08:46 -0500

    Refactoring User class

--- modelUser.py
+++ modelUser.py
@@ -2,20 +2,18 @@
 
 
 class User:
-    def __init__(self, username=None, password=None, next_node=None, _id=None):
+    def __init__(self, _id, username, password):
+        self.id = _id
         self.username = username
         self.password = password
-        self.next_node = next_node
-        self.id = _id
-
-    def get_ids_current(self) -> dict:
-        return {
-            'id': self.id,
-            'next': self.next
-        }
+        self.adjacents = []
 
     def __str__(self) -> str:
-        return f'{self.username},{self.password},{self.id}, {self.next_node}'
+        return f"Username: {self.username}. Password: {self.password}. Adjacents: {self.adjacents}"
 
-    def get_data(self) -> list:
-        return [self.username, self.password, self.id, self.next_node]
+    def to_list(self) -> list:
+        l = list(self.adjacents)
+        l.insert(0, self.password)
+        l.insert(0, self.username)
+        l.insert(0, self.id)
+        return l
